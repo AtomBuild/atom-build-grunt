565f94cf328953da75d100a0f4d572d876284f08
Alexander Olsson <noseglid@gmail.com>
2015-08-28 23:44:20 +0200

    Ported from `atom-build`.

---
+++ spec/build-grunt-spec.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var temp = require('temp');
+var path = require('path');
+var fs = require('fs-extra');
+var specHelpers = require('atom-build-spec-helpers');
+
+describe('grunt provider', function() {
+  var directory;
+  var workspaceElement;
+
+  var setupGrunt = function () {
+    var binGrunt = path.join(directory, 'node_modules', '.bin', 'grunt');
+    var realGrunt = path.join(directory, 'node_modules', 'grunt-cli', 'bin', 'grunt');
+    var source = path.join(__dirname, 'node_modules');
+    var target = path.join(directory, 'node_modules');
+    return specHelpers.vouch(fs.copy, source, target).then(function () {
+      return Promise.all([
+        specHelpers.vouch(fs.unlink, binGrunt),
+        specHelpers.vouch(fs.chmod, realGrunt, parseInt('0700', 8))
+      ]);
+    }).then(function () {
+      return specHelpers.vouch(fs.symlink, realGrunt, binGrunt);
+    });
+  };
+
+  beforeEach(function () {
+    workspaceElement = atom.views.getView(atom.workspace);
+    jasmine.attachToDOM(workspaceElement);
+    jasmine.unspy(window, 'setTimeout');
+    jasmine.unspy(window, 'clearTimeout');
+
+    waitsForPromise(function() {
+      return specHelpers.vouch(temp.mkdir, 'atom-build-spec-').then(function (dir) {
+        return specHelpers.vouch(fs.realpath, dir);
+      }).then(function (dir) {
+        directory = dir + '/';
+        atom.project.setPaths([ directory ]);
+      }).then(function () {
+        return Promise.all([
+          atom.packages.activatePackage('build'),
+          atom.packages.activatePackage('build-grunt')
+        ]);
+      });
+    });
+  });
+
+  it('should show the build panel if a Gruntfile exists', function() {
+    expect(workspaceElement.querySelector('.build')).not.toExist();
+
+    waitsForPromise(setupGrunt);
+
+    runs(function () {
+      fs.writeFileSync(directory + 'Gruntfile.js', fs.readFileSync(__dirname + '/Gruntfile.js'));
+      atom.commands.dispatch(workspaceElement, 'build:trigger');
+    });
+
+    waitsFor(function() {
+      return workspaceElement.querySelector('.build .title') &&
+        workspaceElement.querySelector('.build .title').classList.contains('success');
+    });
+
+    runs(function() {
+      expect(workspaceElement.querySelector('.build')).toExist();
+      expect(workspaceElement.querySelector('.build .output').textContent).toMatch(/Surprising is the passing of time. But not so, as the time of passing/);
+    });
+  });
+
+  it('should run default target if grunt is not installed', function () {
+    fs.writeFileSync(directory + 'Gruntfile.js', fs.readFileSync(__dirname + '/Gruntfile.js'));
+    atom.commands.dispatch(workspaceElement, 'build:trigger');
+
+    waitsFor(function() {
+      return workspaceElement.querySelector('.build .title') &&
+        workspaceElement.querySelector('.build .title').classList.contains('error');
+    });
+
+    runs(function() {
+      expect(workspaceElement.querySelector('.build .output').textContent).toMatch(/^Executing: grunt/);
+    });
+  });
+});
